1caed759e08f9a70561601caf34bf5de1341bdde
Shawn Toffel <shawn.toffel@gmail.com>
2019-02-03 02:15:34 +0000

    Added http handler

---
+++ error.go
@@ -0,0 +1,5 @@
+package contract
+
+type Error struct {
+	Message string `json:"message"`
+}
---
+++ handler.go
@@ -0,0 +1,50 @@
+package contract
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func NewHttpHandler(f func(Request) (*Response, error)) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		request := Request{}
+
+		err := json.NewDecoder(r.Body).Decode(&request)
+		if err != nil {
+			writeError(w, r, err)
+			return
+		}
+
+		response, err := f(request)
+		if err != nil {
+			writeError(w, r, err)
+			return
+		}
+
+		jsonResponse, err := json.Marshal(response)
+		if err != nil {
+			writeError(w, r, err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonResponse)
+	}
+}
+
+func writeError(w http.ResponseWriter, r *http.Request, err error) {
+	responseError := Error{
+		Message: err.Error(),
+	}
+
+	jsonResponse, err := json.Marshal(responseError)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write(jsonResponse)
+}
