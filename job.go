ebe8df4a0fb5c6886199c47a1267337bdbb6bbe1
Shawn Toffel <shawn.toffel@gmail.com>
2019-02-18 09:21:22 +0000

    include schedule on job

--- job.go
+++ job.go
@@ -1,12 +1,22 @@
 package contract
 
 type Job struct {
-	Id          string `json:"id,omitempty"`
-	UserId      string `json:"userId,omitempty"`
-	Source      string `json:"source,omitempty"`
-	Content     string `json:"content,omitempty"`
-	Endpoint    string `json:"endpoint,omitempty"`
-	Destination string `json:"destination,omitempty"`
-	Enabled     bool   `json:"enabled,omitempty"`
-	Created     int32  `json:"created,omitempty"`
+	Id          string   `json:"id,omitempty"`
+	UserId      string   `json:"userId,omitempty"`
+	Source      string   `json:"source,omitempty"`
+	Content     string   `json:"content,omitempty"`
+	Endpoint    string   `json:"endpoint,omitempty"`
+	Destination string   `json:"destination,omitempty"`
+	Schedule    Schedule `json:"schedule,omitempty"`
+	Enabled     bool     `json:"enabled,omitempty"`
+	Created     int32    `json:"created,omitempty"`
+}
+
+type Schedule struct {
+	Minute     string `json:"minute,omitempty"`
+	Hour       string `json:"hour,omitempty"`
+	DayOfMonth string `json:"dayOfMonth,omitempty"`
+	Month      string `json:"month,omitempty"`
+	DayOfWeek  string `json:"dayOfWeek,omitempty"`
+	Year       string `json:"year,omitempty"`
 }
