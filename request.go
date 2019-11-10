a6eb5ceefc552291c5e5b1a55aba34174e940a98
Shawn Toffel <shawn.toffel@gmail.com>
2019-11-10 19:56:32 +0000

    Added more details to server struct

--- request.go
+++ request.go
@@ -19,6 +19,22 @@ type User struct {
 }
 
 type Server struct {
-	Id   string `json:"id,omitempty"`
-	Name string `json:"name,omitempty"`
+	Id              string `json:"id,omitempty"`
+	Name            string `json:"name,omitempty"`
+	OwnerId         string `json:"ownerId,omitempty"`
+	Description     string `json:"description,omitempty"`
+	UserCount       int32  `json:"userCount,omitempty"`
+	IconUrl         string `json:"iconUrl,omitempty"`
+	SystemChannelId string `json:"systemChannelId,omitempty"`
+}
+
+// Server returns the full server for the ServerId
+func (r Request) Server() Server {
+	for _, server := range r.Servers {
+		if server.Id == r.ServerId {
+			return server
+		}
+	}
+
+	return Server{Id: r.ServerId}
 }
