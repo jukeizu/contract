c1123f7d4410a2d0a7e726c58e9196c78f5d591c
Shawn Toffel <shawn.toffel@gmail.com>
2020-04-22 04:11:12 +0000

    Added jobs to Response

--- response.go
+++ response.go
@@ -3,6 +3,7 @@ package contract
 type Response struct {
 	Messages  []*Message  `json:"messages,omitempty"`
 	Reactions []*Reaction `json:"reactions,omitempty"`
+	Jobs      []*Job      `json:"jobs,omitempty"`
 }
 
 func StringResponse(content string) *Response {
