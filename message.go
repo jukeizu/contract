164aa6073c90779b085b2ece1b00a1c270ec7246
Shawn Toffel <shawn.toffel@gmail.com>
2020-05-16 05:46:22 +0000

    Added reactions to Message

--- message.go
+++ message.go
@@ -1,10 +1,11 @@
 package contract
 
 type Message struct {
-	Content          string  `json:"content,omitempty"`
-	Embed            *Embed  `json:"embed,omitempty"`
-	Files            []*File `json:"files,omitempty"`
-	Tts              bool    `json:"tts,omitempty"`
-	IsPrivateMessage bool    `json:"isPrivateMessage,omitempty"`
-	IsRedirect       bool    `json:"isRedirect,omitempty"`
+	Content          string      `json:"content,omitempty"`
+	Embed            *Embed      `json:"embed,omitempty"`
+	Reactions        []*Reaction `json:"reactions,omitempty"`
+	Files            []*File     `json:"files,omitempty"`
+	Tts              bool        `json:"tts,omitempty"`
+	IsPrivateMessage bool        `json:"isPrivateMessage,omitempty"`
+	IsRedirect       bool        `json:"isRedirect,omitempty"`
 }
