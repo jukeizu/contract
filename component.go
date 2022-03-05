19858479cf96e7734e49e76150c4e1d699aad36e
Shawn Toffel <shawn.toffel@gmail.com>
2022-03-05 18:50:38 +0000

    Add components to message & yaml struct tags

--- component.go
+++ component.go
@@ -1,57 +1,57 @@
 package contract
 
 type Components struct {
-	ActionsRows []*ActionsRow `json:"actionsRows"`
-	Buttons     []*Button     `json:"buttons"`
-	SelectMenus []*SelectMenu `json:"selectMenus"`
-	TextInputs  []*TextInput  `json:"textInputs"`
+	ActionsRows []*ActionsRow `json:"actionsRows,omitempty" yaml:"actionsRows,omitempty"`
+	Buttons     []*Button     `json:"buttons,omitempty" yaml:"buttons,omitempty"`
+	SelectMenus []*SelectMenu `json:"selectMenus,omitempty" yaml:"selectMenus,omitempty"`
+	TextInputs  []*TextInput  `json:"textInputs,omitempty" yaml:"textInputs,omitempty"`
 }
 
 type ActionsRow struct {
-	Buttons     []*Button     `json:"buttons"`
-	SelectMenus []*SelectMenu `json:"selectMenus"`
-	TextInputs  []*TextInput  `json:"textInputs"`
+	Buttons     []*Button     `json:"buttons,omitempty" yaml:"buttons,omitempty"`
+	SelectMenus []*SelectMenu `json:"selectMenus,omitempty" yaml:"selectMenus,omitempty"`
+	TextInputs  []*TextInput  `json:"textInputs,omitempty" yaml:"textInputs,omitempty"`
 }
 
 type ComponentEmoji struct {
-	Id       string `json:"id,omitempty"`
-	Name     string `json:"name,omitempty"`
-	Animated bool   `json:"animated,omitempty"`
+	Id       string `json:"id,omitempty" yaml:"id,omitempty"`
+	Name     string `json:"name,omitempty" yaml:"name,omitempty"`
+	Animated bool   `json:"animated,omitempty" yaml:"animated,omitempty"`
 }
 
 type Button struct {
-	Label    string         `json:"label"`
-	Style    uint           `json:"style"`
-	Disabled bool           `json:"disabled"`
-	Emoji    ComponentEmoji `json:"emoji"`
-	Url      string         `json:"url,omitempty"`
-	CustomId string         `json:"customId,omitempty"`
+	Label    string         `json:"label" yaml:"label,omitempty"`
+	Style    uint           `json:"style" yaml:"style,omitempty"`
+	Disabled bool           `json:"disabled" yaml:"disabled,omitempty"`
+	Emoji    ComponentEmoji `json:"emoji" yaml:"emoji,omitempty"`
+	Url      string         `json:"url,omitempty" yaml:"url,omitempty"`
+	CustomId string         `json:"customId,omitempty" yaml:"customId,omitempty"`
 }
 
 type SelectMenuOption struct {
-	Label       string         `json:"label,omitempty"`
-	Value       string         `json:"value"`
-	Description string         `json:"description"`
-	Emoji       ComponentEmoji `json:"emoji"`
-	Default     bool           `json:"default"`
+	Label       string         `json:"label,omitempty" yaml:"label,omitempty"`
+	Value       string         `json:"value" yaml:"value,omitempty"`
+	Description string         `json:"description" yaml:"description,omitempty"`
+	Emoji       ComponentEmoji `json:"emoji" yaml:"emoji,omitempty"`
+	Default     bool           `json:"default" yaml:"default,omitempty"`
 }
 
 type SelectMenu struct {
-	CustomId    string             `json:"customId,omitempty"`
-	Placeholder string             `json:"placeholder"`
-	MinValues   *int               `json:"minValues,omitempty"`
-	MaxValues   int                `json:"maxValues,omitempty"`
-	Options     []SelectMenuOption `json:"options"`
-	Disabled    bool               `json:"disabled"`
+	CustomId    string             `json:"customId,omitempty" yaml:"customId,omitempty"`
+	Placeholder string             `json:"placeholder" yaml:"placeholder,omitempty"`
+	MinValues   *int               `json:"minValues,omitempty" yaml:"minValues,omitempty"`
+	MaxValues   int                `json:"maxValues,omitempty" yaml:"maxValues,omitempty"`
+	Options     []SelectMenuOption `json:"options" yaml:"options,omitempty"`
+	Disabled    bool               `json:"disabled" yaml:"disabled,omitempty"`
 }
 
 type TextInput struct {
-	CustomId    string `json:"customId"`
-	Label       string `json:"label"`
-	Style       uint   `json:"style"`
-	Placeholder string `json:"placeholder,omitempty"`
-	Value       string `json:"value,omitempty"`
-	Required    bool   `json:"required,omitempty"`
-	MinLength   int    `json:"minLength,omitempty"`
-	MaxLength   int    `json:"maxLength,omitempty"`
+	CustomId    string `json:"customId" yaml:"customId,omitempty"`
+	Label       string `json:"label" yaml:"label,omitempty"`
+	Style       uint   `json:"style" yaml:"style,omitempty"`
+	Placeholder string `json:"placeholder,omitempty" yaml:"placeholder,omitempty"`
+	Value       string `json:"value,omitempty" yaml:"value,omitempty"`
+	Required    bool   `json:"required,omitempty" yaml:"required,omitempty"`
+	MinLength   int    `json:"minLength,omitempty" yaml:"minLength,omitempty"`
+	MaxLength   int    `json:"maxLength,omitempty" yaml:"maxLength,omitempty"`
 }
--- message.go
+++ message.go
@@ -1,11 +1,12 @@
 package contract
 
 type Message struct {
-	Content          string   `json:"content,omitempty" yaml:"content,omitempty"`
-	Embed            *Embed   `json:"embed,omitempty" yaml:"embed,omitempty"`
-	Reactions        []string `json:"reactions,omitempty" yaml:"reactions,omitempty"`
-	Files            []*File  `json:"files,omitempty" yaml:"files,omitempty"`
-	Tts              bool     `json:"tts,omitempty" yaml:"tts,omitempty"`
-	IsPrivateMessage bool     `json:"isPrivateMessage,omitempty" yaml:"isPrivateMessage,omitempty"`
-	IsRedirect       bool     `json:"isRedirect,omitempty" yaml:"isRedirect,omitempty"`
+	Content          string      `json:"content,omitempty" yaml:"content,omitempty"`
+	Embed            *Embed      `json:"embed,omitempty" yaml:"embed,omitempty"`
+	Reactions        []string    `json:"reactions,omitempty" yaml:"reactions,omitempty"`
+	Files            []*File     `json:"files,omitempty" yaml:"files,omitempty"`
+	Compontents      *Components `json:"components,omitempty" yaml:"components,omitempty"`
+	Tts              bool        `json:"tts,omitempty" yaml:"tts,omitempty"`
+	IsPrivateMessage bool        `json:"isPrivateMessage,omitempty" yaml:"isPrivateMessage,omitempty"`
+	IsRedirect       bool        `json:"isRedirect,omitempty" yaml:"isRedirect,omitempty"`
 }
